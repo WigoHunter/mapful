bbba29e509614f06d963c8906a235d04f0a82387
ngai tong cheng <ngai tong cheng>
2017-11-07 21:25:15 +0800

    login page ver 1.0(not linked to other pages yet)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Alert, Button, Text, TextInput, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { TabNavigator } from 'react-navigation';
 import { Header } from 'react-native-elements';
@@ -19,7 +19,7 @@ const RootTabs = TabNavigator({
   Discover: {
     screen: () => <Discover />,
     navigationOptions: {
-      title: "DISCOVER",
+      title: "Discover",
       tabBarIcon: <Icon name="star" color="black" />
     }
   },
@@ -27,7 +27,7 @@ const RootTabs = TabNavigator({
   Profile: {
     screen: () => <Home />,
     navigationOptions: {
-      title: "PROFILE",
+      title: "Profile",
       tabBarIcon: <Icon name="face" color="black" />
     }
   }
@@ -38,18 +38,64 @@ const RootTabs = TabNavigator({
   },
 });
 
+const stitch = require("mongodb-stitch")
+const client = new stitch.StitchClient('mapful-cffdt');
+const db = client.service('mongodb', 'mongodb-atlas').db('User');
+client.login().then(() =>
+  console.log("[MongoDB Stitch] Connected to Stitch")
+).catch(err => {
+  console.error(err)
+});
+  
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {username: '',pass:'', id:0};
+		
+	}
+	_onPressLogin() {
+		db.collection('Mapful').find({username:this.state.username,pass:this.state.pass}).then(docs=>{docs.length?(
+		console.log(docs),
+		 Alert.alert('Success')):(Alert.alert('Incorrect username/password!'))})
+	}
+	_onPressRegister() {
+	    db.collection('Mapful').find({username:this.state.username}).then(docs=>docs.length?(Alert.alert('This username has been registered')):(
+		db.collection('Mapful').insert({owner_id : client.authedId(), userid: this.state.id,username: this.state.username,pass:this.state.pass}).catch(err => {console.error(err)	}),
+		this.setState({id:this.state.id+1}),
+		Alert.alert('Registered successfully!')))
+	}
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Header
+      <View style={{ justifyContent: 'center', }}>
+		  {<Header
           outerContainerStyles={{ position: 'relative', top: 0 }}
           innerContainerStyles={{ position: 'relative', top: 0 }}
           leftComponent={{ icon: 'menu', color: '#FFF' }}
           centerComponent={{ text: '', style: { fontSize: 16, color: '#FFF' } }}
           backgroundColor="#1EE494"
-        />
-        <RootTabs />
+		  />}
+		<View style={{flexDirection: 'column',alignItems: 'center', top : 10}} >
+			<Text style={{fontSize :40}}>Mapful</Text>
+		</View>
+		<View style={{ flexDirection: 'column', top : 80}}>
+				<View style={{flexDirection: 'row', top :20,left:60}}>
+						<Text>Username</Text><TextInput  maxLength = {20}  style={{left: 20, width : 150, height: 30, borderColor: 'gray', borderWidth: 2}}  placeholder=" " onChangeText={(username) => this.setState({username})}/>
+				</View>
+				<View style={{ flexDirection: 'row', top :40,left:60}}>
+						<Text>Password</Text><TextInput secureTextEntry={true} maxLength = {20}  style={{left: 20, width : 150, height: 30, borderColor: 'gray', borderWidth: 2}}  placeholder=" " onChangeText={(pass) => this.setState({pass})}/>
+				</View>
+		</View>
+		<View style={{flexDirection: 'row',alignItems: 'center',width:250,top:140,left:100}} >
+			<Button style={{ top :150,left:40,width:10}}
+			onPress={ this._onPressLogin.bind(this)}
+			title="Login"
+			/>
+			<Button style={{ top :150,left:100,width:10}}
+			onPress={this._onPressRegister.bind(this)}
+			title="Register"
+			/>
+		</View>
+  {/*} <RootTabs />*/}
       </View>
     );
   }
