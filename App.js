6754c12001e57c8c67caa0a244d13182c8d2af9a
Kevin Hsu <wigohunter@Kevinde-MacBook-Pro.local>
2017-11-14 17:02:23 +0800

    style login and auto login after register

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Alert, Button, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Alert, Button, Text, TextInput, View, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { TabNavigator } from 'react-navigation';
 import { Header } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
 
 import Home from './components/Home.js';
 import Discover from './components/Discover.js';
@@ -11,19 +12,35 @@ import Pin from './components/Pin.js';
 
 const RootTabs = TabNavigator({
   Home: {
-    screen: Home
+    screen: Home,
+    navigationOptions: {
+      title: 'Home',
+      tabBarIcon: <Icon name="home" color="black" />
+    }
   },
 
   Discover: {
-  screen: Discover
+    screen: Discover,
+    navigationOptions: {
+      title: 'Discover',
+      tabBarIcon: <Icon name="star" color="black" />
+    }
   },
 
   Profile: {
-    screen: Profile
+    screen: Profile,
+    navigationOptions: {
+      title: 'Profile',
+      tabBarIcon: <Icon name="face" color="black" />
+    }
   },
   
   Pin: {
-	screen:  Pin,
+    screen:  Pin,
+    navigationOptions: {
+      title: 'Pin',
+      tabBarIcon: <Icon name="my-location" color="black" />
+    }
   }
 }, {
   tabBarPosition: 'bottom',
@@ -32,7 +49,7 @@ const RootTabs = TabNavigator({
   },
 });
 
-const stitch = require("mongodb-stitch")
+const stitch = require("mongodb-stitch");
 const client = new stitch.StitchClient('mapful-cffdt');
 const db = client.service('mongodb', 'mongodb-atlas').db('Mapful');
 client.login().then(() =>
@@ -44,67 +61,107 @@ client.login().then(() =>
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {username: '',pass:'', id:0,isLoggedIn:false};
-		
-	}
+		this.state = {
+      username: '',
+      pass:'',
+      id:0,
+      isLoggedIn:false
+    };
+  }
+  
 	_onPressLogin() {
-		db.collection('User').find({username:this.state.username,pass:this.state.pass}).then(docs=>{docs.length?(
-		 Alert.alert('Success!'),
-		 this.setState({isLoggedIn:true})):(Alert.alert('Incorrect username/password!'))})
-	}
+    db.collection('User')
+      .find({ username: this.state.username, pass: this.state.pass })
+      .then(docs => { docs.length
+        ? (Alert.alert('Success!'), this.setState({isLoggedIn:true}))
+        : (Alert.alert('Incorrect username/password!'))
+      })
+  }
+  
 	_onPressRegister() {
-		this.state.username==''?(Alert.alert('Username cannot be empty.')):(this.state.pass==''?(Alert.alert('Password cannot be empty.')):(
-	    db.collection('User').find({username:this.state.username}).then(docs=>docs.length?(Alert.alert('This username has been registered')):(
-		db.collection('User').insert({username: this.state.username,pass:this.state.pass}).catch(err => {console.error(err)	}),
-		Alert.alert('Registered successfully!')))))
-	}
+    this.state.username == ''
+      ? (Alert.alert('Username cannot be empty.'))
+      : (this.state.pass == ''
+          ? (Alert.alert('Password cannot be empty.'))
+          : (
+            db.collection('User')
+              .find({ username:this.state.username })
+              .then(docs => docs.length
+                ? (Alert.alert('This username has been registered'))
+                :(
+                  db.collection('User')
+                    .insert({ username: this.state.username, pass: this.state.pass})
+                    .then(_ => {
+                     this._onPressLogin(); 
+                    })
+                    .catch(err => { console.error(err) })
+                )
+              )
+            )
+          )
+  }
+  
   render() {
+    const { isLoggedIn } = this.state;
+
 	  if(!this.state.isLoggedIn){
-		  return(
-      <View style={{ justifyContent: 'center', }}>
-		  {<Header
-          outerContainerStyles={{ position: 'relative', top: 0 }}
-          innerContainerStyles={{ position: 'relative', top: 0 }}
-          leftComponent={{ icon: 'menu', color: '#FFF' }}
-          centerComponent={{ text: '', style: { fontSize: 16, color: '#FFF' } }}
-          backgroundColor="#1EE494"
-		  />}
-		<View style={{flexDirection: 'column',alignItems: 'center', top : 10}} >
-			<Text style={{fontSize :40}}>Mapful</Text>
-		</View>
-		<View style={{ flexDirection: 'column', top : 80}}>
-				<View style={{flexDirection: 'row', top :20,left:60}}>
-						<Text>Username</Text><TextInput  maxLength = {20}  style={{left: 20, width : 150, height: 30, borderColor: 'gray', borderWidth: 2}}  placeholder=" " onChangeText={(username) => this.setState({username})}/>
-				</View>
-				<View style={{ flexDirection: 'row', top :40,left:60}}>
-						<Text>Password</Text><TextInput secureTextEntry={true} maxLength = {20}  style={{left: 20, width : 150, height: 30, borderColor: 'gray', borderWidth: 2}}  placeholder=" " onChangeText={(pass) => this.setState({pass})}/>
-				</View>
-		</View>
-		<View style={{flexDirection: 'row',alignItems: 'center',width:250,top:140,left:100}} >
-			<Button style={{ top :150,left:40,width:10}}
-			onPress={ this._onPressLogin.bind(this)}
-			title="Login"
-			/>
-			<Button style={{ top :150,left:100,width:10}}
-			onPress={this._onPressRegister.bind(this)}
-			title="Register"
-			/>
-		  </View>
-      </View>
-	  )
-	  }else {
-    return (
-		
-      <View  style={{ justifyContent: 'center', flex:1}}>
-		  {<Header
-          outerContainerStyles={{ position: 'relative', top: 0 }}
-          innerContainerStyles={{ position: 'relative', top: 0 }}
-          leftComponent={{ icon: 'menu', color: '#FFF' }}
-          centerComponent={{ text: '', style: { fontSize: 16, color: '#FFF' } }}
-          backgroundColor="#1EE494"
-		  />}
-		  <RootTabs screenProps={{user:this.state.username}} />
-      </View>
-  );}
+		  return (
+        <View style={{ backgroundColor: '#1EE494', flex: 1 }}>
+          <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <Image
+              source={require('./img/mapful.png')}
+              style={{
+                width: 100,
+                height: 100,
+                marginBottom: 20
+              }}
+            />
+            <Text style={{fontSize: 32, marginBottom: 25, color: '#FFF'}}>Mapful</Text>
+            <View style={{ flexDirection: 'row', margin: 10, alignItems: 'center' }}>
+                <Text>Username</Text><TextInput maxLength={20}  style={styles.input}  placeholder="username" onChangeText={(username) => this.setState({username})}/>
+            </View>
+            <View style={{ flexDirection: 'row', margin: 10, alignItems: 'center' }}>
+                <Text>Password</Text><TextInput secureTextEntry={true} maxLength = {20}  style={styles.input}  placeholder="password" onChangeText={(pass) => this.setState({pass})}/>
+            </View>
+
+            <View style={{flexDirection: 'row',alignItems: 'center', justifyContent: 'center', marginTop: 30 }} >
+              <Button style={{ top :150,left:40,width:10}}
+                onPress={ this._onPressLogin.bind(this)}
+                title="Login"
+              />
+              <Button style={{ top :150,left:100,width:10}}
+                onPress={this._onPressRegister.bind(this)}
+                title="Register"
+              />
+            </View>
+          </View>
+        </View>
+    )
+    } else {
+      return (
+        <View  style={{ justifyContent: 'center', flex:1}}>
+          <Header
+              outerContainerStyles={{ position: 'relative', top: 0 }}
+              innerContainerStyles={{ position: 'relative', top: 0 }}
+              leftComponent={{ icon: 'menu', color: '#FFF' }}
+              centerComponent={{ text: '', style: { fontSize: 16, color: '#FFF' } }}
+              backgroundColor="#1EE494"
+          />
+          <RootTabs screenProps={{user:this.state.username}} />
+        </View>
+      );
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 100,
+    marginLeft: 20,
+    width: 150,
+    height: 32,
+    borderColor: '#FFF',
+    borderWidth: 1,
+    paddingLeft: 10
+  }
+})
