153ef16f78797a03d84f19b9d6783a8079daf90c
Kevin Hsu <wigohunter@Kevinde-MacBook-Pro.local>
2017-11-16 02:20:15 +0800

    discover page shows pins of stories with photos

--- components/Discover.js
+++ components/Discover.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, ScrollView, Text, Image } from 'react-native';
 import { Header, Icon } from 'react-native-elements';
 import MapView from 'react-native-maps';
 
@@ -28,6 +28,7 @@ export default class Discover extends React.Component {
     };
 
     this.OnRegionChange = this.OnRegionChange.bind(this);
+    this.updatePins = this.updatePins.bind(this);
   }
 
   componentDidMount() {
@@ -48,13 +49,17 @@ export default class Discover extends React.Component {
       });
     }, Options);
 
-    db.collection('Pins')
-      .find({})
-      .then(pins => this.setState({ pins }));
+    this.updatePins();
   }
 
   OnRegionChange(region) {
     this.setState({ region });
+  }
+
+  updatePins() {
+    db.collection('Pins')
+      .find({})
+      .then(pins => this.setState({ pins }));
   }
 
   render() {
@@ -62,9 +67,31 @@ export default class Discover extends React.Component {
 
     return (
       <View style={{ flex: 1 }}>
+        <View style={{
+          position: 'absolute',
+          zIndex: 100,
+          top: 20,
+          right: 20,
+        }}>
+          <Text
+            onPress={() => this.updatePins()}
+            style={{
+              paddingTop: 5,
+              paddingBottom: 5,
+              paddingRight: 8,
+              paddingLeft: 8,
+              borderRadius: 6,
+              backgroundColor: '#1EE494',
+              color: '#FFF'
+            }}
+          >
+            Update
+          </Text>
+        </View>
         <MapView
           style={{ flex: 1 }}
           region={region}
+          showsCompass={false}
           onRegionChange={this.onRegionChange}
         >
           {pins.map(pin =>
@@ -74,12 +101,69 @@ export default class Discover extends React.Component {
                 latitude: pin.location.latitude,
                 longitude: pin.location.longitude
               }}
-              title={pin.title}
-              description={pin.txt}
-            />
+            >
+              <MapView.Callout style={{ zIndex: 10000 }}>
+                <Callout pin={pin} />
+              </MapView.Callout>
+            </MapView.Marker>
           )}
         </MapView>
       </View>
     );
   }
 }
+
+const Callout = ({ pin }) => (
+  <ScrollView contentContainerStyle={styles.callout}>
+    <Text style={styles.title}>{pin.title}</Text>
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Text style={styles.username}>{pin.username}</Text>
+      <Text style={styles.time}>{`${pin.time.getDate()} / ${pin.time.getMonth()} / ${pin.time.getFullYear()}`}</Text>
+    </View>
+    {(pin.image != null && pin.image.length > 0) &&
+      <Image
+        source={{
+          uri: `https://res.cloudinary.com/comp33302017/image/upload/v${pin.image[0].version}/${pin.image[0].id}`
+        }}
+        style={{
+          width: 200,
+          height: 200,
+          marginBottom: 10,
+          marginTop: 10,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      />
+    }
+    <Text style={styles.txt}>{pin.txt}</Text>
+  </ScrollView>
+);
+
+const styles = StyleSheet.create({
+  callout: {
+    width: '100%',
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    width: 230,
+  },
+
+  username: {
+    fontSize: 14,
+    color: '#1EE494',
+  },
+
+  time: {
+    fontSize: 12,
+    color: '#AAA',
+    marginLeft: 8
+  },
+
+  txt: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    width: 230,
+  }
+});
